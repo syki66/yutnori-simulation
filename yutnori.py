e8f971aebbdb48e3b2fe1ac9e3a3a58a9e68d08a
syki66 <66syki@gmail.com>
2020-11-09 16:20:05 +0900

    add rules

--- yutnori.py
+++ yutnori.py
@@ -1,4 +1,4 @@
-
+# 윷놀이 게임기, 통계적 확률 계산기
 
 # 도 : 0.1536 (15.36%)
 # 개 : 0.3456 (34.56%)
@@ -6,36 +6,39 @@
 # 윷 : 0.1296 (12.96%)
 # 모 : 0.0256 (2.56 %)
 
-# 앞면 : 1, 뒷면 : 0
+# 둥근면 : 1, 평평한면 : 0
 
-# 앞면 : 40% , 뒷면 : 60% (평평한면이 위로 나올 확률이 높음)
+# 둥근면 : 40% , 평평한면 : 60% (평평한면이 위로 나올 확률이 높음)
 
 import random
 
-yut_p = [0, 0, 0, 0, 0]
+count = 1000
 
-count = 1000000
+winning_count = 0
 for _ in range(count):
-    yuts = []
-    for _ in range(4):
-        yuts.append( random.choices(range(0,2), weights=[6,4])[0] )
+
+    player = 0
+
+    while player == 0 or player % 5 != 0:
+
+        yuts = []
+        for _ in range(4):
+            yuts.append(random.choices(range(0, 2), weights=[6, 4])[0])
 
 
+        if yuts.count(0) == 1:
+            player += 1
+        elif yuts.count(0) == 2:
+            player += 2
+        elif yuts.count(0) == 3:
+            player += 3
+        elif yuts.count(0) == 4:
+            player += 4
+        else:
+            player += 5
 
-    if yuts.count(0) == 1:
-        yut_p[0] += 1
-    elif yuts.count(0) == 2:
-        yut_p[1] += 1
-    elif yuts.count(0) == 3:
-        yut_p[2] += 1
-    elif yuts.count(0) == 4:
-        yut_p[3] += 1
-    else:
-        yut_p[4] += 1
+        if player > 50:
+            winning_count += 1
+            break
 
-
-print(f'도 : {yut_p[0] / count}')
-print(f'개 : {yut_p[1] / count}')
-print(f'걸 : {yut_p[2] / count}')
-print(f'윷 : {yut_p[3] / count}')
-print(f'모 : {yut_p[4] / count}')
+print(winning_count, count)
