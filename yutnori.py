57e9b7f90e1374d28014e12ca8ecae0e9873dc0a
syki66 <66syki@gmail.com>
2020-11-09 21:03:18 +0900

    add 10M result

--- yutnori.py
+++ yutnori.py
@@ -1,4 +1,4 @@
-# 윷놀이 기계 확률 계산하기
+# 윷놀이 뽑기 기계 확률 계산기
 
 # 자바스크립트로 변환하기
 
@@ -42,14 +42,24 @@ def playYutnori(destination, play_count):
             if player > (destination * 5):
                 winning_count += 1
                 break
-
     return play_count, winning_count
 
 for i in range(1, 11):
     try:
-        try_count, success_count = playYutnori(i, 1000000)
+        try_count, success_count = playYutnori(i, 100000000)
         percentage = round(success_count / try_count * 100, 4)
         expectation = math.ceil(100 / percentage) * 1000
-        print(f'{i}번 -> 시도 : {try_count}, 성공 : {success_count}, 확률 : {percentage}%, 1개당 뽑기 금액 : {expectation}원')
+        print(f'{i}번 뽑기 -> 시도 : {try_count}, 성공 : {success_count}, 확률 : {percentage}%, 1개당 뽑기 금액 : {expectation}원')
     except ZeroDivisionError:
-        print(f'{i}번 -> 시도 : {try_count}, 성공 : 0, 확률 : 0%')
+        print(f'{i}번 뽑기 -> 시도 : {try_count}, 성공 : 0, 확률 : 0%')
+
+''' 10,000,000번 시뮬레이션 결과
+1번 -> 시도 : 10000000, 성공 : 6111478, 확률 : 61.1148%, 1개당 뽑기 금액 : 2000원
+2번 -> 시도 : 10000000, 성공 : 3664489, 확률 : 36.6449%, 1개당 뽑기 금액 : 3000원
+3번 -> 시도 : 10000000, 성공 : 2199597, 확률 : 21.996%, 1개당 뽑기 금액 : 5000원
+4번 -> 시도 : 10000000, 성공 : 1318975, 확률 : 13.1898%, 1개당 뽑기 금액 : 8000원
+5번 -> 시도 : 10000000, 성공 : 793480, 확률 : 7.9348%, 1개당 뽑기 금액 : 13000원
+6번 -> 시도 : 10000000, 성공 : 476141, 확률 : 4.7614%, 1개당 뽑기 금액 : 22000원
+7번 -> 시도 : 10000000, 성공 : 285225, 확률 : 2.8522%, 1개당 뽑기 금액 : 36000원
+8번 -> 시도 : 10000000, 성공 : 170974, 확률 : 1.7097%, 1개당 뽑기 금액 : 59000원
+'''
